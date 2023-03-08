7fc5286fbcb1ddeca2c9ec699cfeb85fd107cf8f
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2023-03-08 12:17:05 +0530

    Create routes

---
+++ server/routes/workouts.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', () => {})
+
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const workoutRoutes = require('./routes/workouts')
 
 //express app
 const app = express()
