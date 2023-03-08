e41c4c22dc6a9766b1fce900dca32bad93780da0
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2023-03-08 21:00:30 +0530

    data base connection

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 //express app
@@ -21,7 +22,13 @@ app.use((req, res, next) => {
 app.use('/api/workouts', workoutRoutes)
 
 
-//listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT)
-})
+
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        //listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('Connect to the DB and listening on port', process.env.PORT)
+        })
+    })
+    .catch((err) => console.log(err))
