925f019c13aaf664c0800aff4a610c4e37238110
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2023-03-09 22:24:45 +0530

    fetch all datas to frontend

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,30 @@
+import { useEffect, useState } from 'react';
+
+
+//components
+import WorkoutDetails from '../components/WorkoutDetails'
+
 const Home = () => {
+    const [workouts,setWorouts] = useState(null)
+useEffect(() => {
+    const fetchWorkouts = async () => {
+        const response = await fetch('/api/workouts')
+        const json = await response.json()
+
+        if(response.ok) {
+            setWorouts(json)
+        }
+    }
+   fetchWorkouts()
+}, []);
+
     return (
         <div className="home">
-        <h2>Home</h2>
+            <div className="workouts">
+                {workouts && workouts.map((workout) => (
+                    <WorkoutDetails key={ workout._id} workout={workout} />
+                ))} 
+        </div>
         </div>
     )
 }
