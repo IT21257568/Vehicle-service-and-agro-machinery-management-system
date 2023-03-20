22b965b440d007ecfff69be5fbc9a48d8ab888eb
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2023-03-20 20:44:19 +0530

    Carrer section backend part started and CRUD completed

---
+++ server/controllers/vacanciesController.js
@@ -0,0 +1,77 @@
+const Vacancy = require('../models/vacanciesModel');
+const mongoose = require('mongoose');
+
+//get all vacancies
+const getVacancies = async(req, res) => {
+    const vacancies = await Vacancy.find({}).sort({ createdAt: -1 });
+
+    res.status(200).json(vacancies);
+}
+
+//get single vacancy
+const getVacancy = async(req, res) => {
+        const { id } = req.params;
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(404).json({ error: 'Vacancy not found' });
+        }
+
+        const vacancy = await Vacancy.findById(id);
+        if (!vacancy) {
+            return res.status(404).json({ error: 'Vacancy not found' });
+        } else {
+            res.status(200).json(vacancy);
+        }
+
+    }
+    //create new vacancy
+const ceateVacancy = async(req, res) => {
+    const { vacncy_title, vacncy_type, vacancy_count, vacncy_requirements } = req.body;
+    //add to db
+    try {
+        const vacancy = await Vacancy.create({ vacncy_title, vacncy_type, vacancy_count, vacncy_requirements });
+        res.status(200).json({ vacancy });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+
+}
+
+//delete a vacancy
+const deleteVacancy = async(req, res) => {
+        const { id } = req.params;
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(404).json({ error: 'Vacancy not found' });
+        }
+        const vacancy = await Vacancy.findOneAndDelete({ _id: id });
+        if (!vacancy) {
+            return res.status(404).json({ error: 'Vacancy not found' });
+        } else {
+            res.status(200).json({ message: 'Vacancy deleted' });
+        }
+    }
+    //update a vacancy
+const updateVacancy = async(req, res) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'Vacancy not found' });
+    }
+    const vacancy = await Vacancy.findOneAndUpdate({ _id: id }, {
+        ...req.body
+    });
+    if (!vacancy) {
+        return res.status(404).json({ error: 'Vacancy not found' });
+    } else {
+        res.status(200).json(vacancy);
+    }
+}
+
+
+
+module.exports = {
+    ceateVacancy,
+    getVacancies,
+    getVacancy,
+    deleteVacancy,
+    updateVacancy
+
+}
