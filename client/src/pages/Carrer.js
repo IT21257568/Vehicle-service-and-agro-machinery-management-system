abc37afb98996a1739d49bfebacb0f53802b0545
pawan-live <pawan.senpura@gmail.com>
2023-04-06 22:31:39 +0530

    added new dashboard code

--- client/src/pages/Carrer.js
+++ client/src/pages/Carrer.js
@@ -1,34 +1,36 @@
-import { useEffect } from 'react';
-import { useVacancyContext } from '../hooks/useVacancyContext';
-
+import { useEffect } from "react";
+import { useVacancyContext } from "../hooks/useVacancyContext";
 
 //components
-import VacancyDetails from '../components/VacancyDetails'
-import VacancyForm from '../components/VacancyForm'
+import VacancyDetails from "../components/VacancyDetails";
+import VacancyForm from "../components/VacancyForm";
 
 const Carrer = () => {
-    const {vacancies,dispatch} = useVacancyContext()
-useEffect(() => {
+  const { vacancies, dispatch } = useVacancyContext();
+  useEffect(() => {
     const fetchVacancies = async () => {
-        const response = await fetch('/api/vacancies')
-        const json = await response.json()
+      const response = await fetch(
+        `http://localhost:${process.env.REACT_APP_SERVER_PORT}/api/vacancies`
+      );
+      const json = await response.json();
 
-        if(response.ok) {
-            dispatch({type: 'SET_VACANCIES', payload: json})
-        }
-    }
-   fetchVacancies()
-}, [dispatch]);
+      if (response.ok) {
+        dispatch({ type: "SET_VACANCIES", payload: json });
+      }
+    };
+    fetchVacancies();
+  }, [dispatch]);
 
-    return (
-        <div className="carrer">
-            <div className="vacancies">
-                {vacancies && vacancies.map((vacancies) => (
-                    <VacancyDetails key={ vacancies._id} vacancies={vacancies} />
-                ))} 
-            </div>
-            <VacancyForm/>
-        </div>
-    )
-}
-export default Carrer
+  return (
+    <div className="carrer">
+      <div className="vacancies">
+        {vacancies &&
+          vacancies.map((vacancies) => (
+            <VacancyDetails key={vacancies._id} vacancies={vacancies} />
+          ))}
+      </div>
+      <VacancyForm />
+    </div>
+  );
+};
+export default Carrer;
--- server/server.js
+++ server/server.js
@@ -1,34 +1,34 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const mongoose = require('mongoose')
-const vanaciesRoute = require('./routes/vanaciesRoute')
+const express = require("express");
+const mongoose = require("mongoose");
+const vanaciesRoute = require("./routes/vanaciesRoute");
+// setup cors
+const cors = require("cors");
 
 //express app
-const app = express()
+const app = express();
 
 //middleware
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-
-})
-
+  console.log(req.path, req.method);
+  next();
+});
 
 //routes
 //1
-app.use('/api/vacancies', vanaciesRoute)
-
-
+app.use("/api/vacancies", vanaciesRoute);
 
 //connect to db
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        //listen for requests
-        app.listen(process.env.PORT, () => {
-            console.log('Connect to the DB and listening on port', process.env.PORT)
-        })
-    })
-    .catch((err) => console.log(err))
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("Connect to the DB and listening on port", process.env.PORT);
+    });
+  })
+  .catch((err) => console.log(err));
