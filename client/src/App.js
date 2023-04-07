08cf0d846724ac49ce621f3e1fb286f22dd60d25
pawan-live <pawan.senpura@gmail.com>
2023-04-07 12:02:19 +0530

    add fetch job details on updateVacancy page

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //pages & components
-import Home from './pages/Carrer'
-import Navbar from './components/Navbar'
-import UpdateVacancy from './components/updateVacancyForm'
+import Home from "./pages/Carrer";
+import Navbar from "./components/Navbar";
+import UpdateVacancy from "./components/UpdateVacancyForm";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar/>
+        <Navbar />
         <div className="pages">
           <Routes>
-            <Route
-              path = "/" 
-              element={<Home/>}
-            />
-            <Route
-              path = "/UpdateVacancy" 
-              element={<UpdateVacancy/>}
-            />
+            <Route path="/" element={<Home />} />
+            <Route path="/UpdateVacancy/:id" element={<UpdateVacancy />} />
           </Routes>
         </div>
       </BrowserRouter>
--- client/src/components/VacancyDetails.js
+++ client/src/components/VacancyDetails.js
@@ -1,41 +1,54 @@
-import { useVacancyContext } from '../hooks/useVacancyContext';
+import { useVacancyContext } from "../hooks/useVacancyContext";
 
 //date fns
-import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
 
 import { useNavigate } from "react-router-dom";
 
-const VacanyDetails = ({ vacancies }) => { 
-    const {dispatch} = useVacancyContext()
+const VacanyDetails = ({ vacancies }) => {
+  const { dispatch } = useVacancyContext();
 
-    const handleClick = async() => {
-        const response = await fetch('/api/vacancies/' + vacancies._id, {
-            method: 'DELETE'
-        })
-        const json = await response.json()
-        if (response.ok){
-            dispatch({type: 'DELETE_VACANCY', payload: json})
-        }
+  const handleClick = async () => {
+    const response = await fetch("/api/vacancies/" + vacancies._id, {
+      method: "DELETE",
+    });
+    const json = await response.json();
+    if (response.ok) {
+      dispatch({ type: "DELETE_VACANCY", payload: json });
     }
-    let navigate = useNavigate(); 
-    const routeChange = () =>{ 
-      let path = `UpdateVacancy`; 
-      navigate(path);
-    }
+  };
+  let navigate = useNavigate();
+  const routeChange = () => {
+    let path = `UpdateVacancy/${vacancies._id}`;
+    navigate(path);
+  };
 
-    return (
-        <div className="vacancies-details">
-            <h4>{vacancies.vacncy_title}</h4>
-            <p><strong>Vacancy Type: </strong> {vacancies.vacncy_type}</p>
-            <p><strong>Vacancy Count: </strong> {vacancies.vacancy_count}</p>
-            <p><strong>Requiremnts: </strong> {vacancies.vacncy_requirements}</p>
-            <p>{formatDistanceToNow(new Date(vacancies.createdAt), {addSuffix:true})}</p>
-            
-            <button  onClick={routeChange} className="updateBtn">Update Vacancy</button>
-            <button onClick={handleClick} className="deleteBtn">Delete Vacancy</button>
+  return (
+    <div className="vacancies-details">
+      <h4>{vacancies.vacncy_title}</h4>
+      <p>
+        <strong>Vacancy Type: </strong> {vacancies.vacncy_type}
+      </p>
+      <p>
+        <strong>Vacancy Count: </strong> {vacancies.vacancy_count}
+      </p>
+      <p>
+        <strong>Requiremnts: </strong> {vacancies.vacncy_requirements}
+      </p>
+      <p>
+        {formatDistanceToNow(new Date(vacancies.createdAt), {
+          addSuffix: true,
+        })}
+      </p>
 
-        </div>
-    )
-}
+      <button onClick={routeChange} className="updateBtn">
+        Update Vacancy
+      </button>
+      <button onClick={handleClick} className="deleteBtn">
+        Delete Vacancy
+      </button>
+    </div>
+  );
+};
 
-export default VacanyDetails
+export default VacanyDetails;
