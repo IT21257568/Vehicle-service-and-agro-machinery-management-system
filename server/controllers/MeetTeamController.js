408d5e4948e7e4d6af24f2ab7aaab7c292e76261
Nisal <it21259302@my.sliit.lk>
2023-04-15 10:23:01 +0530

    :memo: Correct syntax errors

--- server/controllers/MeetTeamController.js
+++ server/controllers/MeetTeamController.js
@@ -26,43 +26,43 @@ const getmTeam = async(req, res) => {
     //create new Member Profile
 const ceatemTeam = async(req, res) => {
     const {
-        technican_name,
-        technican_age,
-        technican_experiences,
-        technican_expertise,
-        technican_picture_url,
+        technician_name,
+        technician_age,
+        technician_experiences,
+        technician_expertise,
+        technician_picture_url
     } = req.body;
 
     let emptyFields = [];
 
     //validation for empty fields
-    if (!technican_name) {
-      emptyFields.push("vacncy_title");
+    if (!technician_name) {
+      emptyFields.push("technican_name");
     }
-    if (!technican_age) {
-      emptyFields.push("vacncy_type");
+    if (!technician_age) {
+      emptyFields.push("technican_age");
     }
-    if (!technican_experiences) {
-      emptyFields.push("vacancy_count");
+    if (!technician_experiences) {
+      emptyFields.push("technican_experiences");
     }
-    if (!technican_expertise) {
-      emptyFields.push("vacncy_requirements");
+    if (!technician_expertise) {
+      emptyFields.push("technican_expertise");
     }
-    if (!technican_picture_url) {
-      emptyFields.push("vacncy_requirements");
+    if (!technician_picture_url) {
+      emptyFields.push("technican_picture_url");
     }
     if (emptyFields.length > 0) {
         return res.status(400).json({ error: 'Please fill in all fields:', emptyFields });
-    }
+    } 
 
     //add to db
     try {
         const mTeam = await Mteam.create({
-          technican_name,
-          technican_age,
-          technican_experiences,
-          technican_expertise,
-          technican_picture_url,
+          technician_name,
+          technician_age,
+          technician_experiences,
+          technician_expertise,
+          technician_picture_url,
         });
         res.status(200).json({ mTeam });
     } catch (error) {
