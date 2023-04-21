5cb52d47a8c92b706f96abad98c5b93bb02fe190
Nisal <it21259302@my.sliit.lk>
2023-04-21 11:09:26 +0530

    :bug: Ama's validation issue fix

--- server/models/agroModel.js
+++ server/models/agroModel.js
@@ -3,31 +3,31 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const agroSchema = new Schema({
-    p_name:{
+    p_name: {
         type: String,
         required: true
     },
-    p_image:{
+    p_image: {
         type: String,
         required: true
     },
-    p_price:{
+    p_price: {
         type: Number,
         required: true
     },
-    p_discount:{
+    p_discount: {
         type: Number,
         required: true
     },
-    p_description:{
+    p_description: {
         type: String,
         required: true
     },
-    p_status:{
+    p_status: {
         type: String,
         required: true
     },
 
-}, {timestamps: true })
+}, { timestamps: true });
 
 module.exports = mongoose.model('AgroProduct', agroSchema)
