c5d2e7560e5dca6c9948ce3acb4e5f062b4e91e6
nethvish <nethumvishwadinu@gmail.com>
2023-04-22 23:36:21 +0530

    crate and updated user progress status js file and Headre file

---
+++ dashboard/src/components/Headers/ProgressStatusHeader.js
@@ -0,0 +1,34 @@
+import { Button, Container, Row, Col } from "reactstrap";
+
+const ProgressStatusHeader = () => {
+  return (
+    <>
+      <div
+        className="header pb-8 pt-5 pt-lg-8 d-flex align-items-center"
+        style={{
+          minHeight: "500px",
+          backgroundImage:
+            "url(" + require("../../assets/img/theme/progress-tarcking.jpg") + ")",
+          backgroundSize: "contain",
+          
+        }}
+      >
+        {/* Mask */}
+        <span className="mask bg-gradient-default opacity-8" />
+        {/* Header container */}
+        <Container className="d-flex align-items-center" fluid>
+          <Row>
+            <Col lg="7" md="10">
+              <h1 className="display-2 text-white" style={{width: '35rem'}}>Tracking the Progress of Your Car Service Online</h1>
+              <p className="text-white mt-0 mb-5" style={{width: '25rem'}}>
+                Stay Informed with Real-Time Updates on Your Car Service
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
+};
+
+export default ProgressStatusHeader;
